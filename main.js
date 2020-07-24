fa844be35cccd1c7a061a85d3800671bd12361d4
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-07-24 11:48:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -648,7 +648,7 @@ function dark() {
   document.getElementById("b4").style.background = "#737373";
   document.getElementById("bu").style.background = "#737373";
   image = document.getElementById('gollllld');
-  image.src = "Squirrel-314.github.io/Images/goldrushDark.png?time=1595605378186";
+  image.src = "goldrushDark.png";
   //document.getElementById("gain").style.background = "#737373";
   //document.getElementById("gollllld").scr = "file:///C:/Users/theho/OneDrive/Desktop/Gold%20Rush/Images/goldrushDark.png?time=1595009551290";
 
