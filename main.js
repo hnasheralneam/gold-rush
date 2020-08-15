80136c3b26d65f23d04b0c7be7d5f1c578fe4271
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-08-15 08:58:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1097,7 +1097,7 @@ var dwarfNews = [
   'News: Dwarfs rights movment spreading across the globe, dwarfs demand equality and voting rights.',
   'News: Gold mine collapses, dwarfs demand safer workplaces.',
   'News: Hoards of angry dwarfs fill the streets worldwide during dwarf rights protests, "it\'s suprising how threatening a mob of tiny people can be" admits journalist',
-  'News: Peaceful dwarf protests recived with violent reprisal',
+  'News: Peaceful dwarf protests recived with violent reprisal!',
 ]
 var mineNews = [
   'News: Mines opening everywhere, environmentalists worried.',
