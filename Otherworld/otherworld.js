8ba00fa23c1dc5fe2deb8e50d6c21c938364d8d2
Squirrel-314 <schoolarlybay@gmail.com>
2020-12-18 09:18:00 -0500

    hi

--- Otherworld/otherworld.js
+++ Otherworld/otherworld.js
@@ -12,16 +12,16 @@
 function gameSetup() {
    alert("Welcome to the Otherworld. Have a nice trip.")
    var username = prompt ("What shall you be called while you are in the Otherworld?");
-   gameData.playerName = username;
+   otherData.playerName = username;
    console.log("Hello, " + username);
-   document.getElementById("playerrName").innerHTML = "Hail Lord " + gameData.playerName;
+   document.getElementById("playerrName").innerHTML = "Hail Lord " + otherData.playerName;
 }
 
 //==========================================================
 //Game Data
 //==========================================================
 
-var initialGameData = {
+var initialOtherData = {
   otherStars: 0,
   otherStarsPerSecond: 0,
 
@@ -50,15 +50,15 @@ var initialGameData = {
   lastTick: Date.now()
 }
 
-var gameData = initialGameData
+var otherData = initialOtherData
 
 //==========================================================
 //Gain Profit
 //==========================================================
 
 function collectOtherStar() {
-  gameData.otherStars += 1
-  document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
+  otherData.otherStars += 1
+  document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
 }
 
 //==========================================================
@@ -66,113 +66,113 @@ function collectOtherStar() {
 //==========================================================
 
 function exploreSystem() {
-  if (gameData.otherStars >= gameData.exploreSystemCost) {
-    gameData.otherStars -= gameData.exploreSystemCost
-    gameData.otherStarsPerSecond += 5
-    gameData.exploreSystemCost *= 1.5
-    gameData.systemCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreSystem").innerHTML = "Solar System<br> (You have " + gameData.systemCount + ") <br>Cost: " + gameData.exploreSystemCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreSystemCost) {
+    otherData.otherStars -= otherData.exploreSystemCost
+    otherData.otherStarsPerSecond += 5
+    otherData.exploreSystemCost *= 1.5
+    otherData.systemCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreSystem").innerHTML = "Solar System<br> (You have " + otherData.systemCount + ") <br>Cost: " + otherData.exploreSystemCost + " Otherstars"
   }
 }
 function exploreSCluster() {
-  if (gameData.otherStars >= gameData.exploreSClusterCost) {
-    gameData.otherStars -= gameData.exploreSClusterCost
-    gameData.otherStarsPerSecond += 10
-    gameData.exploreSClusterCost *= 1.5
-    gameData.sClusterCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + gameData.sClusterCount + ") <br>Cost: " + gameData.exploreSClusterCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreSClusterCost) {
+    otherData.otherStars -= otherData.exploreSClusterCost
+    otherData.otherStarsPerSecond += 10
+    otherData.exploreSClusterCost *= 1.5
+    otherData.sClusterCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + otherData.sClusterCount + ") <br>Cost: " + otherData.exploreSClusterCost + " Otherstars"
   }
 }
 function exploreSCluster() {
-  if(gameData.otherStars >= gameData.exploreSClusterCost) {
-    gameData.otherStars -= gameData.exploreSClusterCost
-    gameData.otherStarsPerSecond += 15
-    gameData.exploreSClusterCost *= 1.5
-    gameData.sClusterCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + gameData.sClusterCount + ") <br>Cost: " + gameData.exploreSClusterCost + " Otherstars"
+  if(otherData.otherStars >= otherData.exploreSClusterCost) {
+    otherData.otherStars -= otherData.exploreSClusterCost
+    otherData.otherStarsPerSecond += 15
+    otherData.exploreSClusterCost *= 1.5
+    otherData.sClusterCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + otherData.sClusterCount + ") <br>Cost: " + otherData.exploreSClusterCost + " Otherstars"
   }
 }
 function exploreNebula() {
-  if (gameData.otherStars >= gameData.exploreNebulaCost) {
-    gameData.otherStars -= gameData.exploreNebulaCost
-    gameData.otherStarsPerSecond += 50
-    gameData.exploreNebulaCost *= 1.5
-    gameData.nebulaCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreNebula").innerHTML = "Nebula<br> (You have " + gameData.nebulaCount + ") <br>Cost: " + gameData.exploreNebulaCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreNebulaCost) {
+    otherData.otherStars -= otherData.exploreNebulaCost
+    otherData.otherStarsPerSecond += 50
+    otherData.exploreNebulaCost *= 1.5
+    otherData.nebulaCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreNebula").innerHTML = "Nebula<br> (You have " + otherData.nebulaCount + ") <br>Cost: " + otherData.exploreNebulaCost + " Otherstars"
   }
 }
 function exploreBlack() {
-  if (gameData.otherStars >= gameData.exploreBlackCost) {
-    gameData.otherStars -= gameData.exploreBlackCost
-    gameData.otherStarsPerSecond += 125
-    gameData.exploreBlackCost *= 1.5
-    gameData.blackCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreBlack").innerHTML = "Back Hole<br> (You have " + gameData.blackCount + ") <br>Cost: " + gameData.exploreBlackCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreBlackCost) {
+    otherData.otherStars -= otherData.exploreBlackCost
+    otherData.otherStarsPerSecond += 125
+    otherData.exploreBlackCost *= 1.5
+    otherData.blackCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreBlack").innerHTML = "Back Hole<br> (You have " + otherData.blackCount + ") <br>Cost: " + otherData.exploreBlackCost + " Otherstars"
   }
 }
 function exploreGalaxy() {
-  if (gameData.otherStars >= gameData.exploreGalaxyCost) {
-    gameData.otherStars -= gameData.exploreGalaxyCost
-    gameData.otherStarsPerSecond += 300
-    gameData.exploreGalaxyCost *= 1.5
-    gameData.galaxyCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreGalaxy").innerHTML = "Galaxy<br> (You have " + gameData.galaxyCount + ") <br>Cost: " + gameData.exploreGalaxyCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreGalaxyCost) {
+    otherData.otherStars -= otherData.exploreGalaxyCost
+    otherData.otherStarsPerSecond += 300
+    otherData.exploreGalaxyCost *= 1.5
+    otherData.galaxyCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreGalaxy").innerHTML = "Galaxy<br> (You have " + otherData.galaxyCount + ") <br>Cost: " + otherData.exploreGalaxyCost + " Otherstars"
   }
 }
 function explorePCluster() {
-  if (gameData.otherStars >= gameData.explorePClusterCost) {
-    gameData.otherStars -= gameData.explorePClusterCost
-    gameData.otherStarsPerSecond += 500
-    gameData.explorePClusterCost *= 1.5
-    gameData.pClusterCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("explorePCluster").innerHTML = "Protocluster<br> (You have " + gameData.pClusterCount + ") <br>Cost: " + gameData.explorePClusterCost + " Otherstars"
+  if (otherData.otherStars >= otherData.explorePClusterCost) {
+    otherData.otherStars -= otherData.explorePClusterCost
+    otherData.otherStarsPerSecond += 500
+    otherData.explorePClusterCost *= 1.5
+    otherData.pClusterCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("explorePCluster").innerHTML = "Protocluster<br> (You have " + otherData.pClusterCount + ") <br>Cost: " + otherData.explorePClusterCost + " Otherstars"
   }
 }
 function exploreGCluster() {
-  if (gameData.otherStars >= gameData.exploreGClusterCost) {
-    gameData.otherStars -= gameData.exploreGClusterCost
-    gameData.otherStarsPerSecond += 1250
-    gameData.exploreGClusterCost *= 1.5
-    gameData.gClusterCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreGCluster").innerHTML = "Galaxy Cluster<br> (You have " + gameData.gClusterCount + ") <br>Cost: " + gameData.exploreGClusterCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreGClusterCost) {
+    otherData.otherStars -= otherData.exploreGClusterCost
+    otherData.otherStarsPerSecond += 1250
+    otherData.exploreGClusterCost *= 1.5
+    otherData.gClusterCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreGCluster").innerHTML = "Galaxy Cluster<br> (You have " + otherData.gClusterCount + ") <br>Cost: " + otherData.exploreGClusterCost + " Otherstars"
   }
 }
 function exploreGSCluster() {
-  if (gameData.otherStars >= gameData.exploreGSClusterCost) {
-    gameData.otherStars -= gameData.exploreGSClusterCost
-    gameData.otherStarsPerSecond += 5000
-    gameData.exploreGSClusterCost *= 1.5
-    gameData.gSClusterCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreGSCluster").innerHTML = "Galaxy Supercluster<br> (You have " + gameData.gSClusterCount + ") <br>Cost: " + gameData.exploreGSClusterCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreGSClusterCost) {
+    otherData.otherStars -= otherData.exploreGSClusterCost
+    otherData.otherStarsPerSecond += 5000
+    otherData.exploreGSClusterCost *= 1.5
+    otherData.gSClusterCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreGSCluster").innerHTML = "Galaxy Supercluster<br> (You have " + otherData.gSClusterCount + ") <br>Cost: " + otherData.exploreGSClusterCost + " Otherstars"
   }
 }
 function exploreGWall() {
-  if (gameData.otherStars >= gameData.exploreGWallCost) {
-    gameData.otherStars -= gameData.exploreGWallCost
-    gameData.otherStarsPerSecond += 20000
-    gameData.exploreGWallCost *= 1.5
-    gameData.gWallCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreGWall").innerHTML = "Galaxy Walls<br> (You have " + gameData.gWallCount + ") <br>Cost: " + gameData.exploreGWallCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreGWallCost) {
+    otherData.otherStars -= otherData.exploreGWallCost
+    otherData.otherStarsPerSecond += 20000
+    otherData.exploreGWallCost *= 1.5
+    otherData.gWallCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreGWall").innerHTML = "Galaxy Walls<br> (You have " + otherData.gWallCount + ") <br>Cost: " + otherData.exploreGWallCost + " Otherstars"
   }
 }
 function exploreUniverse() {
-  if (gameData.otherStars >= gameData.exploreUniverseCost) {
-    gameData.otherStars -= gameData.exploreUniverseCost
-    gameData.otherStarsPerSecond += 1000000
-    gameData.exploreUniverseCost *= 1.5
-    gameData.universeCount += 1
-    document.getElementById("otherStars").innerHTML = gameData.otherStars + " Otherstars"
-    document.getElementById("exploreUniverse").innerHTML = "Universe <br> (You have " + gameData.universeCount + ") <br>Cost: " + gameData.exploreUniverseCost + " Otherstars"
+  if (otherData.otherStars >= otherData.exploreUniverseCost) {
+    otherData.otherStars -= otherData.exploreUniverseCost
+    otherData.otherStarsPerSecond += 1000000
+    otherData.exploreUniverseCost *= 1.5
+    otherData.universeCount += 1
+    document.getElementById("otherStars").innerHTML = otherData.otherStars + " Otherstars"
+    document.getElementById("exploreUniverse").innerHTML = "Universe <br> (You have " + otherData.universeCount + ") <br>Cost: " + otherData.exploreUniverseCost + " Otherstars"
   }
 }
 
@@ -185,19 +185,19 @@ function exploreUniverse() {
 //}
 
 var mainGameLoop = window.setInterval(function() {
-  diff = Date.now() - gameData.lastTick;
-  gameData.lastTick = Date.now()
-  gameData.otherStars += Math.round(gameData.otherStarsPerSecond  * (diff / 1000))
-  document.getElementById("otherStars").innerHTML = Math.round(gameData.otherStars) + " Otherstars"
+  diff = Date.now() - otherData.lastTick;
+  otherData.lastTick = Date.now()
+  otherData.otherStars += Math.round(otherData.otherStarsPerSecond  * (diff / 1000))
+  document.getElementById("otherStars").innerHTML = Math.round(otherData.otherStars) + " Otherstars"
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem("otherworldSave", JSON.stringify(gameData))
+  localStorage.setItem("otherworldSave", JSON.stringify(otherData))
 }, 15000)
 
 var savegame = JSON.parse(localStorage.getItem("otherworldSave"))
 if (savegame !== null) {
-  gameData = savegame
+  otherData = savegame
 }
 
 //==========================================================
@@ -209,15 +209,15 @@ function restart() {
   if (r == true) {
      var rr = confirm("Are you REALLY SURE you want to restart? There is no going back!");
      if (rr == true) {
-       gameData = initialGameData
-       localStorage.setItem("otherworldSave", JSON.stringify(gameData))
+       otherData = initialOtherData;
+       localStorage.setItem("otherworldSave", JSON.stringify(otherData))
        document.location.href = ("")
      }
   }
 }
 
 function save() {
-  localStorage.setItem("otherworldSave", JSON.stringify(gameData))
+  localStorage.setItem("otherworldSave", JSON.stringify(otherData))
 }
 
 //==========================================================
@@ -226,19 +226,19 @@ function save() {
 
 function gameLayout() {
 //Building Count
-   document.getElementById("exploreSystem").innerHTML = "Solar System<br> (You have " + gameData.systemCount + ") <br>Cost: " + gameData.exploreSystemCost + " Otherstars"
-   document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + gameData.sClusterCount + ") <br>Cost: " + gameData.exploreSClusterCost + " Otherstars"
-   document.getElementById("exploreNebula").innerHTML = "Nebula<br> (You have " + gameData.nebulaCount + ") <br>Cost: " + gameData.exploreNebulaCost + " Otherstars"
-   document.getElementById("exploreBlack").innerHTML = "Back Hole<br> (You have " + gameData.blackCount + ") <br>Cost: " + gameData.exploreBlackCost + " Otherstars"
-   document.getElementById("exploreGalaxy").innerHTML = "Galaxy<br> (You have " + gameData.galaxyCount + ") <br>Cost: " + gameData.exploreGalaxyCost + " Otherstars"
-   document.getElementById("explorePCluster").innerHTML = "Protocluster<br> (You have " + gameData.pClusterCount + ") <br>Cost: " + gameData.explorePClusterCost + " Otherstars"
-   document.getElementById("exploreGCluster").innerHTML = "Galaxy Cluster<br> (You have " + gameData.gClusterCount + ") <br>Cost: " + gameData.exploreGClusterCost + " Otherstars"
-   document.getElementById("exploreGSCluster").innerHTML = "Galaxy Supercluster<br> (You have " + gameData.gSClusterCount + ") <br>Cost: " + gameData.exploreGSClusterCost + " Otherstars"
-   document.getElementById("exploreGWall").innerHTML = "Galaxy Walls<br> (You have " + gameData.gWallCount + ") <br>Cost: " + gameData.exploreGWallCost + " Otherstars"
-   document.getElementById("exploreUniverse").innerHTML = "Universe <br> (You have " + gameData.universeCount + ") <br>Cost: " + gameData.exploreUniverseCost + " Otherstars"
+   document.getElementById("exploreSystem").innerHTML = "Solar System<br> (You have " + otherData.systemCount + ") <br>Cost: " + otherData.exploreSystemCost + " Otherstars"
+   document.getElementById("exploreSCluster").innerHTML = "Star Cluster<br> (You have " + otherData.sClusterCount + ") <br>Cost: " + otherData.exploreSClusterCost + " Otherstars"
+   document.getElementById("exploreNebula").innerHTML = "Nebula<br> (You have " + otherData.nebulaCount + ") <br>Cost: " + otherData.exploreNebulaCost + " Otherstars"
+   document.getElementById("exploreBlack").innerHTML = "Back Hole<br> (You have " + otherData.blackCount + ") <br>Cost: " + otherData.exploreBlackCost + " Otherstars"
+   document.getElementById("exploreGalaxy").innerHTML = "Galaxy<br> (You have " + otherData.galaxyCount + ") <br>Cost: " + otherData.exploreGalaxyCost + " Otherstars"
+   document.getElementById("explorePCluster").innerHTML = "Protocluster<br> (You have " + otherData.pClusterCount + ") <br>Cost: " + otherData.explorePClusterCost + " Otherstars"
+   document.getElementById("exploreGCluster").innerHTML = "Galaxy Cluster<br> (You have " + otherData.gClusterCount + ") <br>Cost: " + otherData.exploreGClusterCost + " Otherstars"
+   document.getElementById("exploreGSCluster").innerHTML = "Galaxy Supercluster<br> (You have " + otherData.gSClusterCount + ") <br>Cost: " + otherData.exploreGSClusterCost + " Otherstars"
+   document.getElementById("exploreGWall").innerHTML = "Galaxy Walls<br> (You have " + otherData.gWallCount + ") <br>Cost: " + otherData.exploreGWallCost + " Otherstars"
+   document.getElementById("exploreUniverse").innerHTML = "Universe <br> (You have " + otherData.universeCount + ") <br>Cost: " + otherData.exploreUniverseCost + " Otherstars"
 
-  document.getElementById("playerrName").innerHTML = "Hail Lord " + gameData.playerName;
-  if (gameData.playerName == 3) {
+  document.getElementById("playerrName").innerHTML = "Hail Lord " + otherData.playerName;
+  if (otherData.playerName == 3) {
     gameSetup();
   }
 }
