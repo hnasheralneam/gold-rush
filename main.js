bab8160a051a68be7c1ddf4122d10fc6f0403e10
Squirrel-314 <thehillsidetownship@gmail.com>
2021-12-01 21:20:06 +0000

    Design Changes and Upgrades Dynamic

--- main.js
+++ main.js
@@ -205,8 +205,8 @@ let upgradeData = initialUpgradeData;
 let goldPerSecond = goldPerSecond => gameData.pickaxeGold + gameData.dwarfGold + gameData.gooseGold + gameData.mineGold + gameData.dragonGold + gameData.stoneGold + gameData.stationGold + gameData.leprechaunGold + gameData.sheepGold + gameData.rayGold + gameData.mergerGold;
 
 // Colors for avilibe or unavalibe buildings
-let regColor = "#a503fc";
-let notEnoughColor = "#4a0078";
+let regColor = "#ffdc7d";
+let notEnoughColor = "#5e4f24";
 
 let alert = document.querySelector(".alert");
 let bonusNumber = 1;
@@ -263,25 +263,54 @@ function acquireAsset(asset, assetCost, costMultiplier) {
 //==========================================================
 
 (function () {
-   createUpgradeElement("b", 1, "pickaxe", "check.png", "Sharper Pickaxes!", "This statement has not been evaluated by the Food and Drug Administration!", "Pickaxe profit x2");
-   createUpgradeElement("b", 2, "pickaxe", "neww.png", "New Element!", "Pickaxium works 3x better than competing brands!", "Pickaxe profit x2");
-   createUpgradeElement("b", 3, "pickaxe", "set.png", "Automatic Mechanisms!", "These pickaxes will mine by themselves!(watching them work may be slightly disconcerting)", "Pickaxe profit x2");
-   createUpgradeElement("b", 4, "pickaxe", "gear.png", "Self-oiling Gears!", "Make them work so fast they blur!(safety gear recommended)", "Pickaxe profit x2");
-   createUpgradeElement("c", 1, "dwarf", "school.png", "Dwarf Academy!", "Complete with posh students!", "Dwarf profit x2");
-   createUpgradeElement("c", 2, "dwarf", "manager.png", "Dwarf Managers!", "Down with the worker unions!", "Dwarf profit x2");
-   createUpgradeElement("c", 3, "dwarf", "caf.png", "Mysterious Cafeteria Food!", "Your workers will become mysteriously obedient!", "Dwarf profit x2");
-   createUpgradeElement("c", 4, "dwarf", "holiday.png", "Paid Holidays!", "There's nothing someone won't do when you give them paid holidays!", "Dwarf profit x2");
-   createUpgradeElement("c0", 1, "goose", "food.png", "Healthier Diets!", "Make sure they get all the nutrients they need!", "Geese profit x2");
-   createUpgradeElement("c0", 2, "goose", "", "Larger Flocks!", "More geese, more eggs!", "Geese profit x2");
-   createUpgradeElement("c0", 3, "goose", "mind.png", "Advanced Mental Abilities!", "Dark magic is your greatest friend!", "Geese profit x2");
-   createUpgradeElement("c0", 4, "goose", "box.png", "Bigger Nesting Boxes!", "I don't even know how this will help!", "Geese profit x2");
-   createUpgradeElement("d", 1, "mine", "", "Deeper Mines!", "Far over, the misty mountains cold... (Read The Hobbit)", "Mine profit x2");
-   createUpgradeElement("d", 2, "mine", "new.png", "Wider Mineshafts!", "Five feet wide the door and three may walk abreast... (Also from The Hobbit)", "Mine profit x2");
-   createUpgradeElement("d", 3, "mine", "globe.png", "Journey to the Center of the Earth!", "(let's hope that isn't copyrighted)", "Mine profit x2");
-   createUpgradeElement("d", 4, "mine", "globe.png", "Holy Earth!", "The Earth is now so riddled with holes, the ground is constantly being torn apart by earthquakes", "Mine profit x2");
+   createUpgradeElement("b", 1, "pickaxe", "pickaxe.png", "Sharper Pickaxes!", "This statement has not been evaluated by the Food and Drug Administration!", "Pickaxe profit x2");
+   createUpgradeElement("b", 2, "pickaxe", "pickaxe.png", "New Element!", "Pickaxium works 3x better than competing brands!", "Pickaxe profit x2");
+   createUpgradeElement("b", 3, "pickaxe", "pickaxe.png", "Automatic Mechanisms!", "These pickaxes will mine by themselves!(watching them work may be slightly disconcerting)", "Pickaxe profit x2");
+   createUpgradeElement("b", 4, "pickaxe", "pickaxe.png", "Self-oiling Gears!", "Make them work so fast they blur!(safety gear recommended)", "Pickaxe profit x2");
+   createUpgradeElement("c", 1, "dwarf", "dwarf.png", "Dwarf Academy!", "Complete with posh students!", "Dwarf profit x2");
+   createUpgradeElement("c", 2, "dwarf", "dwarf.png", "Dwarf Managers!", "Down with the worker unions!", "Dwarf profit x2");
+   createUpgradeElement("c", 3, "dwarf", "dwarf.png", "Mysterious Cafeteria Food!", "Your workers will become mysteriously obedient!", "Dwarf profit x2");
+   createUpgradeElement("c", 4, "dwarf", "dwarf.png", "Paid Holidays!", "There's nothing someone won't do when you give them paid holidays!", "Dwarf profit x2");
+   createUpgradeElement("c0", 1, "goose", "goose.png", "Healthier Diets!", "Make sure they get all the nutrients they need!", "Geese profit x2");
+   createUpgradeElement("c0", 2, "goose", "goose.png", "Larger Flocks!", "More geese, more eggs!", "Geese profit x2");
+   createUpgradeElement("c0", 3, "goose", "goose.png", "Advanced Mental Abilities!", "Dark magic is your greatest friend!", "Geese profit x2");
+   createUpgradeElement("c0", 4, "goose", "goose.png", "Bigger Nesting Boxes!", "I don't even know how this will help!", "Geese profit x2");
+   createUpgradeElement("d", 1, "mine", "mine.png", "Deeper Mines!", "Far over, the misty mountains cold... (Read The Hobbit)", "Mine profit x2");
+   createUpgradeElement("d", 2, "mine", "mine.png", "Smaller Mineshafts!", "Saftey? Comfort? Who cares? Most certiantly not me, sitting here in my mansion. Why, I don't even know if I have clastrophpobia 'cause the smallest space i've ever been is my private limosine.");
+   createUpgradeElement("d", 3, "mine", "mine.png", "Journey to the Center of the Earth!", "(let's hope that isn't copyrighted)", "Mine profit x2");
+   createUpgradeElement("d", 4, "mine", "mine.png", "Holy Earth!", "The Earth is now so riddled with holes, the ground is constantly being torn apart by earthquakes", "Mine profit x2");
+   createUpgradeElement("e", 1, "dragon", "dragon.png", "Dragon Gold Insurance!", "Switch to dragon gold insurance!");
+   createUpgradeElement("e", 2, "dragon", "dragon.png", "Selective Breeding!", "Breed the greediest together, and get GREEDIER DRAGONS!(this may not be a good idea)");
+   createUpgradeElement("e", 3, "dragon", "dragon.png", "Bad Habits!", "Sharing may be caring, but I don't care!");
+   createUpgradeElement("e", 4, "dragon", "dragon.png", "Dragon Disease!", "More gold! MORE GOLD! <strong>MORE GOLD!!!</strong>");
+   createUpgradeElement("f", 1, "stone", "stone.png", "The Flammel Effect!", "Discovered by Nicolas Flammel himself!");
+   createUpgradeElement("f", 2, "stone", "stone.png", "Dark Alchemy!", "Uncover secrets that should not be known!");
+   createUpgradeElement("f", 3, "stone", "stone.png", "Primeval Chemistry!", "Tread paths that have not been tread in a thousand millennia!");
+   createUpgradeElement("f", 4, "stone", "stone.png", "Necromancy!", "HAHAHAHAHA! (That was supposed to be an evil laugh)");
+   createUpgradeElement("g", 1, "station", "station.png", "Solar Power!", "WHY did we not think of this before? All of those double-A Duracell batteries where quite difficult to transport!");
+   createUpgradeElement("g", 2, "station", "station.png", "Self Repairing!", "Through the power of AI!");
+   createUpgradeElement("g", 3, "station", "station.png", "Nanobot Minions!", "Evil at your fingertips! (let the power get to your head!)");
+   createUpgradeElement("g", 4, "station", "station.png", "Space Dust Condenser!", "Create Asteroids!");
+   createUpgradeElement("h", 1, "leprechaun", "leprechaun.png", "Leprechaun Hierarchy!", "Allows for working class!");
+   createUpgradeElement("h", 2, "leprechaun", "leprechaun.png", " 9 Colour Rainbow!", "Infrared, red, orange, goldish, yellow, green, blue, purple, ultraviolet!");
+   createUpgradeElement("h", 3, "leprechaun", "leprechaun.png", "Invisible Cauldrons!", "No more stealing from leprechauns!");
+   createUpgradeElement("h", 4, "leprechaun", "leprechaun.png", "Leprechaun Subuniversal Secrets!", "They are coming, and with them they bring tomatoes.");
+   createUpgradeElement("i", 1, "sheep", "sheep.png", "Bubble Baths!", "Give that fleece a nice good cleaning!");
+   createUpgradeElement("i", 2, "sheep", "sheep.png", "Merino Sheep!", "Only the finest wool for your lordsheep!");
+   createUpgradeElement("i", 3, "sheep", "sheep.png", "Fluffy Paddocks!", "The researchers said it makes more wool!");
+   createUpgradeElement("i", 4, "sheep", "sheep.png", "Alternate Sheep Universe!", "The sheep that come through are not the same, but they have wool!");
+   createUpgradeElement("j", 1, "ray", "ray.png", "Recouped Kinetic Energy!", "MORE POWER!");
+   createUpgradeElement("j", 2, "ray", "ray.png", "Atomic Rearrangement!", "Rearrange those atoms!");
+   createUpgradeElement("j", 3, "ray", "ray.png", "Electron Extraction!", "Collect the electrons! Who cares about radioactive ion waste!");
+   createUpgradeElement("j", 4, "ray", "ray.png", "Proton-Neutron Re-thingy!", "Don't question it, it works!");
+   createUpgradeElement("k", 1, "merger", "merger.png", "Particle Reconstructor!", "Building neutron stars from the ground up!");
+   createUpgradeElement("k", 2, "merger", "merger.png", "Advanced Magnets!", "Increase the collision force!");
+   createUpgradeElement("k", 3, "merger", "merger.png", "Hydrolic Densifier!", "Hydro-powered condensers!");
+   createUpgradeElement("k", 4, "merger", "merger.png", "Strange Star Collisions", "This may cause unexpected results, such as <strong>Otherworldly</strong> occurrences.");
+
 })();
 
-function createUpgradeElement(letter, num, item, src, name, about, info) {
+function createUpgradeElement(letter, num, item, src, name, about) {
    let upgradeBlock = document.createElement("LI");
    upgradeBlock.id = letter + num;
    upgradeBlock.classList.add("upgrade");
@@ -304,7 +333,7 @@ function createUpgradeElement(letter, num, item, src, name, about, info) {
    upgradeBlock.appendChild(why);
    let description = document.createElement("P");
    description.classList.add("upgrade-more");
-   description.textContent = info;
+   description.textContent = `${item} profits x2`;
    upgradeBlock.appendChild(description);
    document.querySelector(".upgradeList").appendChild(upgradeBlock);
 }
@@ -476,7 +505,7 @@ var updateStore = window.setInterval(function() {
    updateDisplay("Dragon", "hire", "Dragon", "A nice dragon to hoard gold for you")
    updateDisplay("Stone", "buy", "Philosopher's Stone", "An alchemy stone that turns ordinary rocks into gold")
    updateDisplay("Station", "open", "Astroid-mining Station", "A space station that mines astroids for gold")
-   updateDisplay("Leprechaun", "hire", "Leprechaun", "Uses magical leprechaun powers to find gold at the end of rainbows")
+   updateDisplay("Leprechaun", "hire", "Leprechaun", "Will find gold at the end of rainbows")
    updateDisplay("Sheep", "hire", "Golden Sheep", "A cute round fluffy sheep with a golden fleece")
    updateDisplay("Ray", "buy", "Mass Ray", "Turns mass into gold")
    updateDisplay("Merger", "buy", "Neutron Star Merger", "Merges neutron stars to create gold (find what you want at it's source ;)")
