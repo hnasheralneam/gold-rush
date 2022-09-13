64b89f7822b1a1180174a0fcf4dff2ab0bd57cd1
Editor Squirrel <thehillsidetownship@gmail.com>
2022-09-13 21:16:46 +0000

    ⚒️ Tweaks

--- app.js
+++ app.js
@@ -5,22 +5,9 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-   res.render("home");
-});
+app.get("/", (req, res) => { res.render("home"); });
+app.get("/play", (req, res) => { res.render("index"); });
+app.get("/mobile", (req, res) => { res.render("mobile"); });
+app.get("/quickplay", (req, res) => { res.render("quickplay"); });
 
-app.get("/play", (req, res) => {
-   res.render("index");
-});
-
-app.get("/mobile", (req, res) => {
-   res.render("mobile");
-});
-
-app.get("/quickplay", (req, res) => {
-   res.render("quickplay");
-});
-
-app.listen(port, () => {
-   console.log(`Listening on port ${port}`);
-});
+app.listen(port, () => { console.log(`Listening...`); });
