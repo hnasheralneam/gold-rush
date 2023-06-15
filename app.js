1f802dc1cbdaf634e4a043e5e5e383f7c860b59b
Editor Rust <thehillsidetownship@gmail.com>
2023-06-15 13:39:32 +0000

    Removed Quickplay Page, Redirects to GitHub Site

--- app.js
+++ app.js
@@ -8,6 +8,5 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => { res.render("home"); });
 app.get("/play", (req, res) => { res.render("index"); });
 app.get("/mobile", (req, res) => { res.render("mobile"); });
-app.get("/quickplay", (req, res) => { res.render("quickplay"); });
 
 app.listen(port, () => { console.log(`Listening...`); });
